fa1b266f8e6930b96375685c846d3e268e30f9cd
idc1407 <idc1407@gmail.com>
2023-08-02 23:06:29 +0530

    s202308022306

---
+++ 4) yavin-website/js/script.js
@@ -0,0 +1,38 @@
+function userScroll() {
+    const navbar = document.querySelector('.navbar');
+    
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+        navbar.classList.add('navbar-sticky');
+        } else {
+        navbar.classList.remove('navbar-sticky');
+        }
+    });
+}
+
+function incrementStats() {
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach((counter) => {
+        counter.innerText = 0;
+
+        const updateCounter = () => {
+            const target = +counter.getAttribute('data-target');
+            const c = +counter.innerText;
+        
+            const increment = target / 200;
+        
+            if (c < target) {
+                counter.innerText = Math.ceil(c + increment);
+                setTimeout(updateCounter, 1);
+            } else {
+                counter.innerText = target;
+            }
+        };
+
+        updateCounter();
+    });
+}
+
+document.addEventListener('DOMContentLoaded', userScroll);
+document.addEventListener('DOMContentLoaded', incrementStats);
