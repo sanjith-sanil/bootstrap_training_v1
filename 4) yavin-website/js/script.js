9f177f9a5a26f790b26aa53454dd7d07aeccfde5
idc1407 <idc1407@gmail.com>
2023-08-03 23:04:34 +0530

    s202308032304

--- 4) yavin-website/js/script.js
+++ 4) yavin-website/js/script.js
@@ -1,19 +1,27 @@
 function userScroll() {
     const navbar = document.querySelector('.navbar');
+    const toTopBtn = document.querySelector('#to-top')
     
     window.addEventListener('scroll', () => {
         if (window.scrollY > 50) {
-        navbar.classList.add('navbar-sticky');
+            navbar.classList.add('navbar-sticky');
+            toTopBtn.classList.add('show');
         } else {
-        navbar.classList.remove('navbar-sticky');
+            navbar.classList.remove('navbar-sticky');
+            toTopBtn.classList.remove('show');
         }
     });
 }
 
-function incrementStats() {
-const counters = document.querySelectorAll('.counter');
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 
-counters.forEach((counter) => {
+function incrementStats() {
+    const counters = document.querySelectorAll('.counter');
+
+    counters.forEach((counter) => {
         counter.innerText = 0;
 
         const updateCounter = () => {
@@ -34,5 +42,7 @@ counters.forEach((counter) => {
     });
 }
 
+// Event Listeners
 document.addEventListener('DOMContentLoaded', userScroll);
 document.addEventListener('DOMContentLoaded', incrementStats);
+document.querySelector('#to-top').addEventListener('click', scrollToTop);
