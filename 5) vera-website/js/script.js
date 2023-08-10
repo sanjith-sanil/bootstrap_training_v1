d499e79aa50e7e6630de3ed3a67db07eaed8cae8
idc1407 <idc1407@gmail.com>
2023-08-10 23:02:22 +0530

    s202308102302

--- 5) vera-website/js/script.js
+++ 5) vera-website/js/script.js
@@ -13,21 +13,39 @@ if (checkReplace !== null) {
 
 // User Scroll For Navbar
 function userScroll() {
-    const navbar = document.querySelector('.navbar');
-  
-    window.addEventListener('scroll', () => {
-      if (window.scrollY > 50) {
-        navbar.classList.add('bg-dark');
-        navbar.classList.add('border-bottom');
-        navbar.classList.add('border-secondary');
-        navbar.classList.add('navbar-sticky');
-      } else {
-        navbar.classList.remove('bg-dark');
-        navbar.classList.remove('border-bottom');
-        navbar.classList.remove('border-secondary');
-        navbar.classList.remove('navbar-sticky');
-      }
-    });
-  }
+  const navbar = document.querySelector('.navbar');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add('bg-dark');
+      navbar.classList.add('border-bottom');
+      navbar.classList.add('border-secondary');
+      navbar.classList.add('navbar-sticky');
+    } else {
+      navbar.classList.remove('bg-dark');
+      navbar.classList.remove('border-bottom');
+      navbar.classList.remove('border-secondary');
+      navbar.classList.remove('navbar-sticky');
+    }
+  });
+}
 
 document.addEventListener('DOMContentLoaded', userScroll);
+
+// Video  Modal
+const videoBtn = document.querySelector('.video-btn')
+const videoModal = document.querySelector('#videoModal')
+const video = document.querySelector('#video')
+let videoSrc;
+
+if (videoBtn !== null) {
+  videoBtn.addEventListener('click', () => {
+    videoSrc = videoBtn.getAttribute('data-bs-src');
+  })
+}
+
+if (videoModal !== null) {
+  videoModal.addEventListener('shown.bs.modal', () => {
+
+  });
+}
